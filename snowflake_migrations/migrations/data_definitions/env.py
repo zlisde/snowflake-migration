ecea107e7e6807f58664bc06fb480fccd934ea64
zlisde <zhengyi.li@curology.com>
2021-07-07 22:07:09 -0700

    inital commit

--- snowflake_migrations/migrations/data_definitions/env.py
+++ snowflake_migrations/migrations/data_definitions/env.py
@@ -14,7 +14,9 @@ class SnowflakeImpl(DefaultImpl):
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+config.set_section_option(
+    section, "SNOWFLAKE_URL", os.environ.get("SNOWFLAKE_URL")
+)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
