ef06f6856e563df72eb72bdee1430468a04308ff
zlisde <zhengyi.li@curology.com>
2021-07-08 10:17:16 -0700

    fix env

--- snowflake_migrations/migrations/data_definitions/env.py
+++ snowflake_migrations/migrations/data_definitions/env.py
@@ -1,7 +1,8 @@
 import os
+import pathlib
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
 from sqlalchemy import pool
 
 from alembic import context
@@ -15,10 +16,7 @@ class SnowflakeImpl(DefaultImpl):
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-section = config.config_ini_section
-config.set_section_option(
-    section, "SNOWFLAKE_URL", os.environ.get("SNOWFLAKE_URL")
-)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
@@ -36,6 +34,11 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+def get_url():
+    sf_url = os.getenv("SNOWFLAKE_URL")
+    sf_database = os.getenv("SNOWFLAKE_CUROLOGY_DATABASE")
+    sf_schema = pathlib.Path(__file__).parent.resolve()
+    return "&".join([sf_url, f"database={sf_database}", f"schema={sf_schema}"])
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -49,7 +52,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -75,12 +78,7 @@ def run_migrations_online():
             if script.upgrade_ops.is_empty():
                 directives[:] = []
 
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
+    connectable = create_engine(get_url())
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
