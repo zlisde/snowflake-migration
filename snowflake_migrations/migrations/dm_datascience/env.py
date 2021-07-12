7a35923f144d3efb692aa2ffbfac68d8c51158c5
zlisde <zhengyi.li@curology.com>
2021-07-12 11:51:17 -0700

    test change file7

--- snowflake_migrations/migrations/dm_datascience/env.py
+++ snowflake_migrations/migrations/dm_datascience/env.py
@@ -55,7 +55,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -81,12 +81,7 @@ def run_migrations_online():
             if script.upgrade_ops.is_empty():
                 directives[:] = []
 
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
+    connectable = create_engine(get_url())
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
